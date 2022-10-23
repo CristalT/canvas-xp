beb61db7a4188dcf32f5114b72345857520865b7
Marcelo J Forclaz <marcelo.forclaz@stensul.com>
2022-10-23 19:24:00 -0300

    Add Canvas class unit tests

--- test/board/canvas.test.ts
+++ test/board/canvas.test.ts
@@ -1,16 +1,27 @@
-import { beforeAll, describe, expect, it } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { Canvas } from 'board';
 
 describe('Canvas', () => {
-    let selector: HTMLDivElement;
-    beforeAll(() => {
+    let selector: HTMLDivElement | null;
+    beforeEach(() => {
         selector = document.createElement('div');
     });
+    afterEach(() => {
+        selector = null;
+    })
 
     it('initializes with default values', () => {
         const canvas = new Canvas(selector);
         expect(canvas.width).toBe(1024);
         expect(canvas.height).toBe(768);
-        expect(selector.innerHTML).toContain('canvas');
+        expect(selector!.innerHTML).toEqual('<canvas width="1024" height="768"></canvas>');
     });
+
+    it('sets background color', () => {
+        const canvas = new Canvas(selector)
+        canvas.context.fillRect = vi.fn()
+        canvas.setBackground('red');
+        expect(canvas.ctx.fillStyle).toEqual('#ff0000')
+        expect(canvas.context.fillRect).toBeCalledWith(0, 0, 1024, 768)
+    })
 });
